6b3f912d3165b58c1c44f54c6fc2505bae74fe86
信息部 <xinxibu@xinxibudeiMac.local>
2015-06-12 09:50:00 +0800

    Initial Commit

---
+++ leso/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  leso
+//
+//  Created by 信息部 on 15/6/12.
+//  Copyright (c) 2015年 信息部. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
